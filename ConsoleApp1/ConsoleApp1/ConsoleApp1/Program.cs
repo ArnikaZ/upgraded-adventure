db7d303341daef77c8367749de39a4b59e6ec6d6
Dominik Żydek <dominik.zydek@edu.uekat.pl>
2023-10-12 07:59:24 +0200

    dodano, chyba w koncu działa

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> histogram = new Dictionary<char, int>();
+
+            Console.WriteLine("Podaj łańcuch znaków: ");
+            string lancuchWejsciowy = Console.ReadLine();
+
+            foreach (char c in lancuchWejsciowy.ToLower())
+            {
+                if (histogram.ContainsKey(c))
+                {
+                    histogram[c]++;
+                }
+                else histogram.Add(c, 1);
+            }
+
+            foreach (var kvp in histogram)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
