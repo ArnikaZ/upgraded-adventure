edee593600b98be6e6842d1710ee48f833ca7841
Dominik Żydek <dominik.zydek@edu.uekat.pl>
2023-10-12 10:32:35 +0200

    zamienilem to co w main na metode

--- ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,10 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Dictionary<char, int> histogram = new Dictionary<char, int>();
-
             Console.WriteLine("Podaj łańcuch znaków: ");
             string lancuchWejsciowy = Console.ReadLine();
+
+            Dictionary<char, int> histogram = Program.Histogram(lancuchWejsciowy);
+
+            foreach (var kvp in histogram)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void PoliczWybraneZnaki(char[] znaki)
+        {
+
+        }
+
+        public static Dictionary<char, int> Histogram(string lancuchWejsciowy) // generuje histogram
+        {
+            Dictionary<char, int> histogram = new Dictionary<char, int>();
 
             foreach (char c in lancuchWejsciowy.ToLower())
             {
@@ -24,12 +41,7 @@ namespace ConsoleApp1
                 else histogram.Add(c, 1);
             }
 
-            foreach (var kvp in histogram)
-            {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-            }
-
-            Console.ReadKey();
+            return histogram;
         }
     }
 }
