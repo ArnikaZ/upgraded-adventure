97946b5d0e0bb00944af737c822d859f0de61940
ArnikaZ <arnika.zabron@gmail.com>
2023-10-14 12:50:21 +0200

    dodanie menu z wyborem wczytania tekstu z klawiatury lub z pliku

--- ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,40 +11,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Podaj łańcuch znaków: ");
-            string lancuchWejsciowy = Console.ReadLine();
+            Menu.StartMenu();
+            
+        }
 
+        public static void WyswietlHistogram(string lancuchWejsciowy)
+        {
             // wyswietla histogram
             Dictionary<char, int> histogram = Program.Histogram(lancuchWejsciowy);
 
             foreach (var kvp in histogram)
             {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
             }
-            Console.WriteLine();
-
-            // pobiera litery i zamienia je w tablice charow
-            Console.Write("Podaj po przecinku wybrane litery: ");
-            string[] znakiStringArray = Console.ReadLine().Split(',');
-            char[] znakiCharArray = new char[znakiStringArray.Length];
-
-            for (int i = 0; i < znakiStringArray.Length; i++)
-            {
-                znakiCharArray[i] = Char.Parse(znakiStringArray[i].Trim());
-            }
-
-            // wyswietla histogram z wybranymi znakami
-            Dictionary<char, int> histogramZWybranymiZnakami = Program.HistogramZWybranymiZnakami(lancuchWejsciowy, znakiCharArray);
-
-            foreach (var kvp in histogramZWybranymiZnakami)
-            {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-            }
-            Console.WriteLine();
-
-            Console.ReadKey();
+             Console.WriteLine();
         }
-
+       
+        
         public static Dictionary<char, int> HistogramZWybranymiZnakami(string lancuchWejsciowy, char[] znaki)
         {
             Dictionary<char, int> histogram = Program.Histogram(lancuchWejsciowy);
